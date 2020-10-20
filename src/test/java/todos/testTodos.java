e0d2a52f45e31090753acb627b8e1e1d8269acf7
Felix Simard <felixsimard@gmail.com>
2020-10-20 16:00:52 -0400

    Working on todos endpoint

--- src/test/java/todos/testTodos.java
+++ src/test/java/todos/testTodos.java
@@ -1,5 +1,41 @@
 package todos;
 
+import org.junit.Test;
+
+import io.restassured.*;
+import io.restassured.http.ContentType;
+import io.restassured.response.*;
+import io.restassured.specification.*;
+import junit.framework.Assert;
+
+import static io.restassured.RestAssured.*;
+
 public class testTodos {
+	
+	String url = "http://localhost:4567/";
+	String endpoint = "todos";
+	
+	String api = url + endpoint;
+	
+	@Test
+	public void testCreateTodoWithValidParameters() {
+			
+		String title = "Wash dishes";
+		String description = "Must not forget to wash dishes.";
+		
+		Response res = given().urlEncodingEnabled(true)
+							.param("title", title)
+							.param("description", description)
+							.header("Accept", "application/json")
+				            .contentType("application/x-www-form-urlencoded")
+							.post(api);
+		
+		ResponseBody body = res.getBody();
+		String bodyAsString = body.asString();
+		System.out.println(bodyAsString);
+		Assert.assertEquals(bodyAsString.contains("dishes"),  true);
+		
+	}
+
 
 }
