40882f510de737835e2fc679e5801b69069aec64
Tyler Watson <tylerjrwatson@gmail.com>
2020-10-21 18:03:38 -0400

    One valid project test

--- src/test/java/APITests/ProjectsTests.java
+++ src/test/java/APITests/ProjectsTests.java
@@ -1,22 +1,44 @@
 package APITests;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.json.simple.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class ProjectsTests {
 
-    @Before
-    public ProjectsTests
+    public ProjectsTests() {
+        RestAssured.baseURI = "http://localhost:4567/projects";
+
+    }
 
     @Test
     public void testCreateProjectValidInfo() {
 
-        String requestBody = withValidInfo("proj1", false, true, "this is proj 1");
+        String title = "proj1";
+        boolean completed = false;
+        boolean active = true;
+        String description = "this is a proj";
 
-        given()
+        String requestBody = withValidInfo(title, completed, active, description);
+
+        RequestSpecification request = given();
+
+        request.header("Content-Type", "application/json");
+        request.header("Accept", "application/json");
+        request.body(requestBody);
+
+        request.post().then().
+                assertThat().
+                body("title", equalTo(title),
+                        "completed", equalTo(String.valueOf(completed)),
+                        "active", equalTo(String.valueOf(active)),
+                        "description", equalTo(String.valueOf(description)));
 
 
     }
