a03e3dbc9d29aa626c46f7645ed68cc1fe7b1a25
APorporino <anthony.porporino21@gmail.com>
2020-10-21 23:39:56 -0400

    Added new helper file for Todo test, and started some tests in TodosTest class.

--- src/test/java/APITests/TodosTests.java
+++ src/test/java/APITests/TodosTests.java
@@ -1,6 +1,7 @@
 package APITests;
 
 import org.json.simple.JSONObject;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,7 +10,6 @@ import io.restassured.http.ContentType;
 import io.restassured.http.Method;
 import io.restassured.response.*;
 import io.restassured.specification.RequestSpecification;
-import junit.framework.Assert;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.Assert.assertEquals;
@@ -19,87 +19,142 @@ import java.util.Map;
 
 public class TodosTests {
 	
+	int statusCodeSuccess = 200;
+	int statusCodeSuccessCreated = 201;
+
+	/**
+	 * Will clear the data and set up baseURI before each test
+	 * 
+	 */
 	@Before 
 	public void setUp() {
 		RestAssured.baseURI = "http://localhost:4567";
+		
+		// must figure out how to delete all data here
+		// 
+		// ShutdownTests.restartApplication();
 	}
-	
+
+	/**
+	 * Will test the endpoint GET /todos - get all todos created
+	 */
 	@Test
 	public void getAllTodos() {
 		
+		RequestSpecification request = RestAssured.given();
+		
+		Response response = request.get("/todos");
+		
+		// assert the response is succesfull 
+		int statusCode = response.getStatusCode();
+		assertEquals(statusCodeSuccess, statusCode);
 	}
-	
+
+	/**
+	 * Will test if a todo can be found by it's ID in the URL
+	 */
 	@Test
 	public void getTodoById() {
-		
+		try {
+			// call helper function to make todo and return id
+			int todoID = TodosHelper.createTodo();
+			RequestSpecification request = RestAssured.given();
+			Response response = request.get("/todos/" + todoID);
+			
+			// assert the response is successful 
+			int statusCode = response.getStatusCode();
+			assertEquals(statusCodeSuccess, statusCode);
+			
+			//not sure why this is NULL but in testCreateTodoValidInfo it isn't
+			String responseBodyTitle = response.jsonPath().get("title");
+			
+			System.out.println("K: " + response.body().jsonPath().get("title"));
+			System.out.println("WHy: " + responseBodyTitle);
+			
+			assertEquals(TodosHelper.todoTitle, responseBodyTitle);
+			int responseID = Integer.parseInt((String) response.jsonPath().get("id"));
+			assertEquals(responseID, todoID);
+			
+		}catch (Exception e){
+			System.out.println(e);
+			Assert.fail();
+		}
 	}
 	
+
+	/**
+	 * Will attempt to create a valid todo with a title in body
+	 */
 	@Test
 	public void testCreateTodoValidInfo() {
-		
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("title", "play guitar");
+
+		String title = "Must complete ECSE429 project";
+
+		RequestSpecification request = RestAssured.given();
+
 		JSONObject requestParams = new JSONObject();
-		requestParams.put("title", "play guitar");
-		
-		Response response = given()
-			.body(requestParams.toJSONString())
-			.when()
-			.post("/todos").then().statusCode(200).extract().response();
-		
-		String titleResponse = response.jsonPath().getString("title");
-		assertEquals(titleResponse,"play guitar");
+		requestParams.put("title", title);
+
+		request.body(requestParams.toJSONString());
+
+		Response response = request.post("/todos");
+
+		// assertions 
+		int statusCode = response.getStatusCode();
+		assertEquals(statusCodeSuccessCreated, statusCode);
+		String responseBodyTitle = response.jsonPath().get("title");
+		assertEquals(responseBodyTitle, title);
 	}
-	
+
 	@Test 
 	public void testCreateToDoAllInfo() {
-		
+
 	}
-	
+
 	@Test 
 	public void testCreateTodoNoTitle() {
-		
+
 	}
-	
+
 	@Test
 	public void testAddTodoToProject() {
-		
+
 	}
-	
+
 	@Test
 	public void testAddTodoToNonExistingProject() {
-		
+
 	}
-	
+
 	@Test
 	public void testRemoveTodoFromProject() {
-		
+
 	}
-	
+
 	@Test
 	public void testGetAllTodoProjects() {
-		
+
 	}
-	
+
 	@Test
 	public void testAddTodoToCategory() {
-		
+
 	}
-	
+
 	@Test
 	public void testAddTodoToNonExistingCategory() {
-		
+
 	}
-	
+
 	@Test
 	public void testRemoveTodoFromCategory() {
-		
+
 	}
-	
+
 	@Test
 	public void testGetAllTodoCategories() {
-		
+
 	}
-	
-	
+
+
 }
